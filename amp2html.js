10f080ceb304cb4486d5f693a01d5078d996e004
Daniel Aleksandersen <code@daniel.priv.no>
2017-10-16 14:35:45 +0200

    Version 1.1.0

---
+++ amp2html.js
@@ -0,0 +1,10 @@
+if (document.querySelector("html[amp],html[⚡]")) {
+  var amp = document.head.querySelector("link[rel='amphtml'][href]"),
+  canon = document.head.querySelector("link[rel='canonical'][href]");
+  if (null != canon && canon.href != null &&
+      !(amp != null && amp.href == canon.href)) {
+    canon = canon.href.trim();
+    if (null != canon &&
+        (canon.startsWith('https:') || canon.startsWith('http:'))) {
+    document.location = canon;
+} } }
