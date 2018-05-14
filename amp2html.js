b65a1292649540d907081e5f362ca85c2ab63896
Daniel Aleksandersen <code@daniel.priv.no>
2018-05-14 23:27:14 +0200

    Updated for redesigned Google News and Search

--- amp2html.js
+++ amp2html.js
@@ -4,17 +4,43 @@
  * License-Filename: LICENSE
  */
 
-// tests for AMP first, and then Google hosted AMP
-if (document.querySelector("html[amp],html[⚡]") ||
-    (document.location.pathname.split('/')[1] == "amp" &&
-     document.location.hostname.split('.')[1] == "google") ) {
+
+// standard AMP documents
+if (document.querySelector("html[amp],html[⚡]"))
+{
   var amp = document.head.querySelector("link[rel='amphtml'][href]"),
   canon = document.head.querySelector("link[rel='canonical'][href],link[rel='canonical']");
   if (null != canon && canon.href != null &&
-      !(amp != null && amp.href == canon.href)) {
+      !(amp != null && amp.href == canon.href))
+  {
     canon = canon.href.trim();
     if (null != canon &&
         document.location != canon &&
-        (canon.startsWith('https:') || canon.startsWith('http:'))) {
-    document.location.replace(canon);
+        (canon.startsWith('https:') || canon.startsWith('http:')))
+    {
+      document.location.replace(canon);
 } } }
+
+// google news
+else if (document.location.host.includes('news.google.'))
+{
+  var amp_links = document.querySelectorAll("a[ampo_url]");
+  for (i = 0; i < amp_links.length; ++i)
+  {
+    var link = amp_links[i];
+    link.onclick = function(){window.stop();document.location.replace(this.href)};
+} }
+
+// google search
+else if (document.location.host.includes('.google.') &&
+    document.querySelector("html[itemtype='http://schema.org/SearchResultsPage']"))
+{
+  var amp_links = document.querySelectorAll("a[data-amp][data-amp-cur]");
+  for (i = 0; i < amp_links.length; ++i)
+  {
+    var link = amp_links[i];
+    link.href = link.dataset.amp;
+    delete link.dataset.amp;
+    delete link.dataset.ampCur;
+} }
+
