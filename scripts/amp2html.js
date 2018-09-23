8e8e962e956fd2c9cdd8be12952679d921635cda
Daniel Aleksandersen <code@daniel.priv.no>
2018-09-23 22:00:16 +0200

    Better handling of AMP Viewers

--- scripts/amp2html.js
+++ scripts/amp2html.js
@@ -40,7 +40,7 @@ if (null != link_amp && null != link_can &&
 } }
 
 // google news
-else if (document.location.host.includes('news.google.'))
+if (document.location.host.includes('news.google.'))
 {
   var amp_links = document.querySelectorAll("a[ampo_url]");
   for (i = 0; i < amp_links.length; ++i)
@@ -50,15 +50,18 @@ else if (document.location.host.includes('news.google.'))
 } }
 
 // google search
-else if (document.location.host.includes('.google.') &&
+if (document.location.host.includes('.google.') &&
     document.querySelector("html[itemtype='http://schema.org/SearchResultsPage']"))
 {
   var amp_links = document.querySelectorAll("a[data-amp][data-amp-cur]");
   for (i = 0; i < amp_links.length; ++i)
   {
     var link = amp_links[i];
-    link.href = link.dataset.amp;
-    delete link.dataset.amp;
-    delete link.dataset.ampCur;
+    link.href = link.dataset.ampCur;
+    link.dataset.amp = link.dataset.ampCur;
+/*    delete link.dataset.ampCur;
+    delete link.dataset.ampTitle;
+    delete link.dataset.ampVed;
+    delete link.dataset.ampVgi; */
 } }
 
--- scripts/redirector.js
+++ scripts/redirector.js
@@ -1,6 +1,38 @@
+// Bing AMP Viewer and Google AMP Viewer
+// Convert https://www.bing.com/amp/s/www.example.com/amp/document
+//      to https://www.example.com/amp/document
+// Convert https://www.google.com/amp/www.example.com/amp/document
+//      to http://www.example.com/amp/document
+
+function amp_viewer_redirector(requestDetails)
+{
+  var amp_viewer = new URL(requestDetails.url);
+  var redirection = amp_viewer.pathname.replace(/^\/amp\/s\//, 'https://').replace(/^\/amp\//, 'http://');
+  browser.storage.local.get('redirect_count').then(
+    data => browser.storage.local.set({'redirect_count': (data.redirect_count || 0) + 1})
+  );
+  return {
+    redirectUrl: redirection
+  };
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  amp_viewer_redirector,
+  {
+    urls: [
+      'https://www.bing.com/amp/*',
+      'https://www.google.com/amp/*'
+    ]
+  },
+  ["blocking"]
+);
+
+
+// Twitter Shortlinks
 // Convert https://t.co/abc123?amp=1
 //      to https://t.co/abc123
-function t_co_redirector(requestDetails) {
+function t_co_redirector(requestDetails)
+{
   var redirection = new URL(requestDetails.url);
   redirection.search = '';
   browser.storage.local.get('redirect_count').then(
@@ -10,7 +42,6 @@ function t_co_redirector(requestDetails) {
     redirectUrl: redirection.toString()
   };
 }
-
 
 browser.webRequest.onBeforeRequest.addListener(
   t_co_redirector,
